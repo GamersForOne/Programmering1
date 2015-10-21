d2a2c3e4f65127f11ac9dbf3b417d58cdd0a3669
GamersForOne <gamersforone@gmail.com>
2015-10-21 20:27:58 +0200

    Second Commit
    Added Files, first one was just .gitignore

---
+++ Nr1-LuhnAlgoritm/src/com/kirdow/luhnalgoritm/LuhnAlgorithm.java
@@ -0,0 +1,76 @@
+package com.kirdow.luhnalgoritm;
+
+public class LuhnAlgorithm {
+	
+	//We need no constructor, so we leave it empty
+	public LuhnAlgorithm() { }
+	
+	//This is the public method, it takes number as input and returns true if it's a valid luhn number
+	//We also work with long (or 64-bit) numbers, because the personnumber seem to not fit in a 32-bit number.
+	public boolean checkLuhn(long number) {
+		//First we generate a pre-luhn number, which is the 2 digit number that
+		//should be evenly divided by 10 (where preluhn % 10 == 0 should return true)
+		long genPreLuhn = this.generatePreLuhn(number);
+		//Then we return true if it's an evenly dividable by 10.
+		return genPreLuhn % 10 == 0;
+	}
+	
+	//this will generate the pre-luhn number, it takes in a long variable as input
+	private long generatePreLuhn(long sequence) {
+		//This line checks if we should start with multiplying by 2 or 1
+		//Remember, we start from the smallest end, so we need to know if it's even or odd amount
+		//of digits, so that the first (highest) digit, is getting multiplied by 2
+		boolean oddTens = digits(sequence) % 2 == 1;
+		//This is the bool that determine if we multiply by 2 or 1
+		boolean doubler = oddTens;
+		//After each iteration, we divide by 10, so we make a backup just in case
+		long numb = sequence;
+		//This is the value that we actually multiply, we set it to -1 to help the loop determining
+		//when the first round is
+		long nMod = -1;
+		//Then we use a stringbuilder, which is better programming practice, because adding to a normal string
+		//Can cause too much garbage
+		StringBuilder sb = new StringBuilder();
+		//This is the loop
+		do {
+			//First it checks if it's not the first time looping, remember that we set the nMod variable to -1
+			//to tell the loop that it's the first time
+			//If it isn't the first time, we jump to the next digit by dividing by 10
+			if (nMod != -1) numb = numb / 10;
+			//This is the one getting the value to multiply, we get the lowest current digit, by dividing the whole by 10
+			//and retrieving the rest (modulus)
+			nMod = numb % 10;
+			//Then we append the value to the StringBuilder, remember that we are alternating doubling and not doubling.
+			//That's what the doubler variable is for
+			sb.append(doubler ? (nMod * 2) : (nMod));
+			//Then we turns doubler to the opposite value, so if it's false, it becomes true, and if it's true, it becomes false
+			doubler = !doubler;
+		//We do this loop when the whole number is greater than 9, the reason we do 9, is because when it's between 10 and 19,
+		//and we divide by 10, it becomes between 0 and 9, and that's the last time we are supposed to loop
+		} while (numb > 9);
+		//Then we create a char array, with the length of the stringbuilder
+		//and then copying over the characters
+		//We could also have just done "char[] preAdd = sb.toString().toCharArray(); but that would just creating an extra
+		//String object which we don't need, so this should save us memory
+		char[] preAdd = new char[sb.length()];
+		//This copies over the chars from the strigbulder to the array
+		sb.getChars(0, sb.length(), preAdd, 0);
+		//Then we initialize the preluhn value, which will add all those values's digits we just created together to get one value
+		long preLuhn = 0;
+		//We loop through all characters
+		for (char c : preAdd)
+			//Here we add the values to the preLuhn value
+			//But hey, where does the 48 come from? Well, in ASCII, the 0 character has the ASCII value, 48, and 1 character has 49 etc
+			//So we subtract 48 to get the actual value and not the character. We could also just parse it, but parsing
+			//Does cause abit of extra memory, so I prefer doing this
+			preLuhn += c - 48;
+		//Now we return the preLuhn value again
+		return preLuhn;
+		
+	}
+	
+	private int digits(long number) {
+		return Long.toString(number).length();
+	}
+	
+}
---
+++ Nr1-LuhnAlgoritm/src/com/kirdow/luhnalgoritm/Main.java
@@ -0,0 +1,44 @@
+package com.kirdow.luhnalgoritm;
+
+import javax.swing.JOptionPane;
+
+//This program is made for Java 8 (version 1.8)
+public class Main {
+	
+	//This is the main method for the program
+	public static void main(String[] args) {
+		//We create a new LuhnAlgorithm object to get access to the algorithm
+		LuhnAlgorithm luhn = new LuhnAlgorithm();
+		//We declare a new string for the input.
+		String input = null;
+		//We start a loop so we don't need to run the program all the time we want to enter a new value
+		do {
+			//We check if input not equals null, if it is, it's because we haven't got the input box yet, we just declared it
+			if (input != null) {
+				//We add a try-catch statement just in case the user entered a non-number value
+				try {
+					//We parse the string to a long, this is where we need the try-catch statement
+					long inputNumber = Long.parseLong(input);
+					//Now we run the algorithm to see if it is a valid luhn number
+					if (luhn.checkLuhn(inputNumber))
+						//If it is, inform the user about it
+						JOptionPane.showMessageDialog(null, "That's a valid Luhn Number");
+					else
+						//If not, inform the user about that too
+						JOptionPane.showMessageDialog(null, "Invalid luhn number");
+					//Did the user not enter a number?
+				} catch (NumberFormatException e) {
+					//If so, prompt the user saying they need to enter a number
+					JOptionPane.showMessageDialog(null, "Oops, you didn't enter a number");
+				}
+			}
+			//This is where the user enters a number
+			input = JOptionPane.showInputDialog("Enter number to check with LuhnAlgorithm");
+		//If the input is null, aka that the user pressed Cancel, return out of the program
+		} while (input != null);
+		//Prompt the user and write in console that the program has ended
+		JOptionPane.showMessageDialog(null, "Program Ended");
+		System.out.println("Program ended");
+	}
+	
+}
